16d8a58a4dce89a390a39839139923797952dc6f
chris.casella <chris.casella@gmail.com>
2017-06-26 13:39:30 -0400

    Completed Devise Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:contact_first_name, :contact_last_name, :company_name, :company_website])
+  end
 end
