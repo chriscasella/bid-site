1ac6fbec4ef789eca397ec6c53b65485d2d59b0e
chris.casella <chris.casella@gmail.com>
2017-06-29 11:53:40 -0400

    Updated React AuctionList.js for pagination

--- react/src/components/auctionList.js
+++ react/src/components/auctionList.js
@@ -6,10 +6,26 @@ class AuctionList extends Component {
   constructor(props){
     super(props)
     this.state = {
-      auctions: []
+      auctions: [],
+      currentPage: 1,
+      auctionsPerPage: 15,
+      search: ''
     }
     this.getData = this.getData.bind(this);
+    this.previousPage = this.previousPage.bind(this);
+   this.nextPage = this.nextPage.bind(this);
+   this.updateSearch = this.updateSearch.bind(this);
   }
+
+  previousPage () {
+    let newPage = this.state.currentPage - 1;
+   this.setState({ currentPage: newPage })
+  }
+
+  nextPage(event) {
+  let newPage = this.state.currentPage + 1;
+  this.setState({ currentPage: newPage })
+}
 
   getData() {
   fetch('http://localhost:3000/api/v1/auctions.json')
@@ -33,7 +49,13 @@ class AuctionList extends Component {
     this.getData();
   }
 
+  updateSearch(event) {
+   this.setState({ search: event.target.value.substr(0,20) })
+ }
+
   render () {
+    let indexOfLastAuction = this.state.auctions * this.state.auctionsPerPage;
+    let indexOfFirstAuction = indexOfLastAuction - this.state.auctionsPerPage;
     let currentAuctions = this.state.auctions;
     let finalAuctions = currentAuctions.map((auction, index) => {
     return (
