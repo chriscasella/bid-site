2febc29801cba309b89c3908c3c4e355a8ad656f
chris.casella <chris.casella@gmail.com>
2017-06-30 14:22:17 -0400

    Added Test

---
+++ spec/features/auctions/user_creates_auction_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature "users can create a new auction" do
+  let!(:user) do
+    User.create(
+    company_name: "Company A",
+    contact_first_name: "Chris",
+    contact_last_name: "Casella",
+    company_website: "www.testsite.com",
+    email: "chris@test.com",
+    password: "password"
+    )
+  end
+  scenario "User creates auction" do
+    sign_in_as(user)
+    visit root_path
+    click_link "Create Auction"
+    fill_in "Auction Name", with: "Lead Drinking Water Survey"
+    fill_in "Location", with: "Camden, NJ"
+    fill_in "Description", with: "Test Description"
+    click_link "Create Auction"
+
+    expect(page).to have_content "Lead Drinking Water Survey"
+    expect(page).to have_content "Camden, NJ"
+    expect(page).to have_content "Test Description"
+  end
+end
