37ae95b2c6315c2a358b26bf1430b9e70c446c18
chris.casella <chris.casella@gmail.com>
2017-06-30 11:59:37 -0400

    Added description line to spec test

--- spec/features/auctions/user_sees_auction_spec.rb
+++ spec/features/auctions/user_sees_auction_spec.rb
@@ -5,6 +5,7 @@ feature "users can view a show page" do
     Auction.create(
     name: "Auction1 Test",
     location: "Philadelphia, PA",
+    description: "This is a test description for the auction",
     workforce_size: 20,
     auction_start_date: Time.now,
     auction_close_date: Time.now
@@ -15,6 +16,7 @@ feature "users can view a show page" do
 
     expect(page).to have_content auction1.name
     expect(page).to have_content auction1.location
+    expect(page).to have_content auction1.description
     expect(page).to have_content auction1.workforce_size
     expect(page).to have_content auction1.auction_start_date
     expect(page).to have_content auction1.auction_close_date
