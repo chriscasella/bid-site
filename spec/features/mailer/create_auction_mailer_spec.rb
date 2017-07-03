5e8a9f9e29ee0d2ceac4f93d02645736e6e6710d
chris.casella <chris.casella@gmail.com>
2017-07-03 19:29:44 -0400

    Actionmailler TDD

---
+++ spec/features/mailer/create_auction_mailer_spec.rb
@@ -0,0 +1,21 @@
+feature "User recieves mail upon auction creation" do
+  let(:user) do
+    FactoryGirl.create(:user)
+  end
+  before(:each)do
+  ActionMailer::Base.delivery_method = :test
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.deliveries = []
+end
+scenario "review a product" do
+
+  sign_in_as(user)
+  visit new_auction_path
+  fill_in "Name of Auction", with: "Lead Drinking Water Survey"
+  fill_in "Location", with: "Camden, NJ"
+  fill_in "Description", with: "Test Description"
+  click_button "Create Auction"
+
+  expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
+end
