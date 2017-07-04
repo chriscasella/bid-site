e3f2d4a49ee6e6e63afc84dd1add5b331e84b164
chris.casella <chris.casella@gmail.com>
2017-07-04 13:15:54 -0400

    Gemfile change, added MailCatcher

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,12 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+    # Tell Action Mailer not to deliver emails to the real world.
+    # The :test delivery method accumulates sent emails in the
+    # ActionMailer::Base.deliveries array.
+    config.action_mailer.delivery_method = :test
+    config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
