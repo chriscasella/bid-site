7c4e7f0821011b94db3a00056f79dfa822a6b724
chris.casella <chris.casella@gmail.com>
2017-07-06 10:49:47 -0400

    Seeds changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,15 @@
 
 Auction.destroy_all
 Bid.destroy_all
+User.destroy_all
 
 user1 = User.create(company_name: "Test User", contact_first_name: "Alan", contact_last_name: "Watts", email: "test@test.com", password: "password")
 user2 = User.create(company_name: "Company Test Name", contact_first_name: "Martin", contact_last_name: "Heidegger", email: "whyanything@test.com", password: "password")
 user3 = User.create(company_name: "Second Test Name", contact_first_name: "Thich Nhat", contact_last_name: "Hanh", email: "plumvillage@test.com", password: "password")
-auction1 = Auction.create(name: "Lead Water Testing", location: "Philadelpia, PA", description: "Lead water testing services needed to conduct environmental assessment of lead u/mG in drinking water. All water fixtures to be labeled as per EPA regulations. Building plans to be included to perform fixture survey.", auction_start_date: Time.now, auction_close_date: Time.now, user_id: 6)
-auction2 = Auction.create(name: "Camden BOE Lead Paint Survey", location: "Camden, NJ", auction_start_date: Time.now, auction_close_date: Time.now, user_id: 6)
-auction3 = Auction.create(name: "Hazardous Materials Remediation and Disposal", location: "Kearny, NJ", auction_start_date: Time.now, auction_close_date: Time.now, user_id: 6)
-auction4 = Auction.create(name: "Federal Wildlife National Refuge Soil Investigation", location: "Littel Egg Harbor, NJ", auction_start_date: Time.now, auction_close_date: Time.now, user_id: 6)
-auction5 = Auction.create(name: "Phase I Site Assessment", location: "Easton, PA", auction_start_date: Time.now, auction_close_date: Time.now, user_id: 7)
-bid1 = Bid.create(user_id: 10, auction_id: auction1, bid_quote: 30000, winning_bid: false)
-bid2 = Bid.create(user_id: 11, auction_id: auction1, bid_quote: 50000, winning_bid: false)
+auction1 = Auction.create(name: "Lead Water Testing", location: "Philadelpia, PA", description: "Lead water testing services needed to conduct environmental assessment of lead u/mG in drinking water. All water fixtures to be labeled as per EPA regulations. Building plans to be included to perform fixture survey.", auction_start_date: Time.now, auction_close_date: Time.now, user: user2)
+auction2 = Auction.create(name: "Camden BOE Lead Paint Survey", location: "Camden, NJ", auction_start_date: Time.now, auction_close_date: Time.now, user: user3)
+auction3 = Auction.create(name: "Hazardous Materials Remediation and Disposal", location: "Kearny, NJ", auction_start_date: Time.now, auction_close_date: Time.now, user: user3)
+auction4 = Auction.create(name: "Federal Wildlife National Refuge Soil Investigation", location: "Littel Egg Harbor, NJ", auction_start_date: Time.now, auction_close_date: Time.now, user: user1)
+auction5 = Auction.create(name: "Phase I Site Assessment", location: "Easton, PA", auction_start_date: Time.now, auction_close_date: Time.now, user: user2)
+bid1 = Bid.create(user: user1, auction: auction1, bid_quote: 30000, winning_bid: false)
+bid2 = Bid.create(user: user2, auction: auction1, bid_quote: 50000, winning_bid: false)
